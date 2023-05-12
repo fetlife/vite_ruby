034bffb32a939c0a51c45486d2fda981d1410a81
Andrii Dmytrenko <refresh.xss@gmail.com>
2023-05-12 12:40:48 +0100

    Update legacy code to match vue-plugin-legacy 4.0.3

--- vite_plugin_legacy/lib/vite_plugin_legacy/tag_helpers.rb
+++ vite_plugin_legacy/lib/vite_plugin_legacy/tag_helpers.rb
@@ -3,8 +3,18 @@
 # Public: Allows to render HTML tags for scripts and styles processed by Vite.
 module VitePluginLegacy::TagHelpers
   VITE_SAFARI_NOMODULE_FIX = <<-JS.html_safe.freeze
-  !function(){var e=document,t=e.createElement("script");if(!("noModule"in t)&&"onbeforeload"in t){var n=!1;e.addEventListener("beforeload",(function(e){if(e.target===t)n=!0;else if(!e.target.hasAttribute("nomodule")||!n)return;console.log('preventing load',e.target);e.preventDefault()}),!0),t.type="module",t.src=".",e.head.appendChild(t),t.remove()}}();
+  !function(){var e=document,t=e.createElement("script");if(!("noModule"in t)&&"onbeforeload"in t){var n=!1;e.addEventListener("beforeload",(function(e){if(e.target===t)n=!0;else if(!e.target.hasAttribute("nomodule")||!n)return;e.preventDefault()}),!0),t.type="module",t.src=".",e.head.appendChild(t),t.remove()}}();
   JS
+  LEGACY_POLYFILL_ID = 'vite-legacy-polyfill'
+  LEGACY_ENTRY_ID = 'vite-legacy-entry'
+  DETECT_MODERN_BROWSER_VAR_NAME = '__vite_is_modern_browser'
+  DETECT_MODERN_BROWSER_DETECTOR = <<-JS.html_safe.freeze
+  import.meta.url;import("_").catch(()=>1);async function* g(){};
+  JS
+  DETECT_MODERN_BROWSER_CODE = <<-JS.html_safe.freeze
+  #{DETECT_MODERN_BROWSER_DETECTOR}if(location.protocol!="file:"){window.#{DETECT_MODERN_BROWSER_VAR_NAME}=true}
+  JS
+
 
   # Renders code to load vite entrypoints for legacy browsers:
   # * Safari NOMODULE fix for Safari 10, which supports modules but not `nomodule`
@@ -16,20 +26,26 @@ module VitePluginLegacy::TagHelpers
     return if ViteRuby.instance.dev_server_running?
 
     tags = []
+    # TODO: we need to inject modern polyfills (legacy-polyfills) only if present
+    # // 2. inject Safari 10 nomodule fix
     safari_nomodule_fix = content_tag(:script, nil, nomodule: true) { VITE_SAFARI_NOMODULE_FIX }
     tags.push(safari_nomodule_fix)
+    # // 3. inject legacy polyfills
     # for browsers which do not support modules at all
-    legacy_polyfill = content_tag(:script, nil, nomodule: true, id: 'vite-legacy-polyfill', src: vite_asset_path('legacy-polyfills', type: :virtual))
+    legacy_polyfill = content_tag(:script, nil, nomodule: true, id: LEGACY_POLYFILL_ID, src: vite_asset_path('legacy-polyfills-legacy', type: :virtual))
     tags.push(legacy_polyfill)
-    # for browsers which support modules, but don't support dynamic import
-    legacy_fallback_tag = content_tag(:script, nil, type: 'module') do
-      vite_dynamic_fallback_inline_code(entrypoints)
-    end
+    # // 4. inject legacy entry
     entrypoints.each do |name, asset_type|
       import_tag = content_tag(:script, nomodule: true) do
-        vite_legacy_import_body(name, asset_type: asset_type)
+        vite_system_js_inline_code(name, asset_type: asset_type)
       end
       tags.push(import_tag)
+    end
+    # for browsers which support modules, but don't support dynamic import or new syntax (safari 11.1)
+    tags.push(content_tag(:script, nil, type: 'module') { DETECT_MODERN_BROWSER_CODE })
+    #  // 5. inject dynamic import fallback entry
+    legacy_fallback_tag = content_tag(:script, nil, type: 'module') do
+      vite_dynamic_fallback_inline_code(entrypoints)
     end
     tags.push(legacy_fallback_tag)
     safe_join(tags, "\n")
@@ -37,15 +53,15 @@ module VitePluginLegacy::TagHelpers
 
   def vite_dynamic_fallback_inline_code(entrypoints)
     load_body = entrypoints.map do |name, asset_type|
-      vite_legacy_import_body(name, asset_type: asset_type)
+      vite_system_js_inline_code(name, asset_type: asset_type)
     end
     load_body = safe_join(load_body, "\n")
     # rubocop:disable Layout/LineLength
-    %{!function(){try{new Function("m","return import(m)")}catch(o){console.warn("vite: loading legacy build because dynamic import is unsupported, syntax error above should be ignored");var e=document.getElementById("vite-legacy-polyfill"),n=document.createElement("script");n.src=e.src,n.onload=function(){#{ load_body }},document.body.appendChild(n)}}();}.html_safe
+    %{!function(){if(window.#{DETECT_MODERN_BROWSER_VAR_NAME})return;console.warn("vite: loading legacy chunks, syntax error above and the same error below should be ignored");var e=document.getElementById("#{LEGACY_POLYFILL_ID}"),n=document.createElement("script");n.src=e.src,n.onload=function(){#{ load_body }},document.body.appendChild(n)}();}.html_safe
     # rubocop:enable Layout/LineLength
   end
 
-  def vite_legacy_import_body(name, asset_type: :javascript)
+  def vite_system_js_inline_code(name, asset_type: :javascript)
     "System.import('#{ vite_asset_path(vite_legacy_name(name), type: asset_type) }')".html_safe
   end
 
